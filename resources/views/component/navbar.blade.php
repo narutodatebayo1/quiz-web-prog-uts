bd5a16e3522ababfcd1fc551e845be159ff3bdec
Timothy Purnawan <timothy.purnawan@binus.edu>
2024-11-07 12:01:26 +0700

    add styling to navbar

--- resources/views/component/navbar.blade.php
+++ resources/views/component/navbar.blade.php
@@ -5,23 +5,23 @@
         </div>
         <div class="nav navbar-brand">
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('home') }}">Home</a>
+                <a class="nav-link {{ Route::is('home') ? 'text-primary text-decoration-underline' : 'text-black' }}" href="{{ route('home') }}">Home</a>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Category</a>
+                <a class="nav-link dropdown-toggle {{ Route::is('category') ? 'text-primary text-decoration-underline' : 'text-black' }}" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Category</a>
                 <ul class="dropdown-menu">
                     <li><a class="dropdown-item" href="{{ route('category', ['id' => 1]) }}">Data Science</a></li>
                     <li><a class="dropdown-item" href="{{ route('category', ['id' => 2]) }}">Network Security</a></li>
                 </ul>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('writers') }}">Writers</a>
+                <a class="nav-link {{ Route::is('writers') ? 'text-primary text-decoration-underline' : 'text-black' }}" href="{{ route('writers') }}">Writers</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('about') }}">About Us</a>
+                <a class="nav-link {{ Route::is('about') ? 'text-primary text-decoration-underline' : 'text-black' }}" href="{{ route('about') }}">About Us</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('popular') }}">Popular</a>
+                <a class="nav-link {{ Route::is('popular') ? 'text-primary text-decoration-underline' : 'text-black' }}" href="{{ route('popular') }}">Popular</a>
             </li>
         </div>
     </div>
